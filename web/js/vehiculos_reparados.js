697128aba426c3859862171da2a7da55bbed7137
BlendBoy <BlendBoy@78ca8046-1e3d-11df-bf30-11b4956ef39d>
2010-04-14 09:18:16 +0000

    Sustituido jqueryUI 7 por jqueryUI 8. Actualizar referencias! avances en los dialogos para agregar/editar vehiculos importados

--- web/js/vehiculos_reparados.js
+++ web/js/vehiculos_reparados.js
@@ -1,7 +1,27 @@
 var PHP_BACKEND_SCRIPT = "php/vehiculos_reparados.php";
 
 $(document).ready(function(){
-	loadVehiculos();	
+	loadComboAnios();
+	loadMarcas();
+	loadVehiculos();
+	
+	$(".add-button").click(function(){
+		$("#addEditDialog").dialog({
+			modal: true,
+			height: 400,
+			width: 500,
+			title: "Agregar nuevo automóvil"
+		});
+	});
+	
+	$(".edit-button").click(function(){
+		$("#addEditDialog").dialog({
+			modal: true,
+			height: 400,
+			width: 500,
+			title: "Editar automóvil existente"
+		});
+	});  
 });
 
 
@@ -17,4 +37,16 @@ function loadVehiculos(){
 			$(".prize").formatCurrency();
 		}
 	);
+}
+
+function loadComboAnios(){
+	var currentYear = new Date().getFullYear();
+	for(var i=currentYear-20; i<=currentYear; i++)
+		$("#cmbAnio").append($("<option>").val(i).html(i));
+}
+
+function loadMarcas(){
+	$("#txtMarca").autocomplete({
+		source: PHP_BACKEND_SCRIPT + "&action=listm"
+	});
 }
